58a39c6ed7b5431f3907bf3903673610ff3e8360
Silvert Felix <felix.silvert.04@gmail.com>
2024-01-07 11:10:17 +0100

    import du projet

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,402 @@
+'use client';
+
 import Image from 'next/image'
+import Link from 'next/link'
+import imgTree from '../public/tree.png';
+import imgKid from '../public/kid.png';
+import imgCity from '../public/city.png';
+import imgCountry from '../public/country.png';
+import imgPlanet from '../public/planet.png';
+import imgCircle from '../public/circle.png';
+
+import { tree } from 'next/dist/build/templates/app-page';
+import { useState } from 'react';
+
+//Afficher un +1 à chaque clic et message d'erreur pour les upgrade
+//Créer une map 
+//Updrage sur les clic
+
+
 
 export default function Home() {
+
+  let x = 0
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <div className='principal'>
+        <div className='leftPrincipal'>
+          <h1>My forest :</h1> <br /> 
+          Max size: <br /> 
+          <span className="maxSize">0</span> trees<br /> 
+          <span className="hectareSize">0</span> hectares <br /> <br /> 
+          CO2 captured per year: <br />
+          <span className="nbCO2">0</span> kg/year<br />
+        </div>
+
+        <div className='midPrincipal'>
+          <div className='titre'>
+            <h1>Trees replanted: <span className="nbCounter">0</span></h1>
+            <h1>per second: <span className="nbPerSecond">0</span></h1>
+          </div>
+          <div className="img">
+            <Image 
+              src={imgTree}
+              width={300}
+              alt="" 
+              onClick={() => incrementCounter()}
+              draggable="false"
             />
-          </a>
+          </div>
+          <div className="lvlSection">
+            LEVEL {x}
+          </div>
+          <div className="xpSection">
+            XP to reach the next level: 50
+          </div>
+        </div>
+        <div className='rightPrincipal'>
+{/*         <Image 
+          src={imgCircle}
+          width={circleSize}
+          alt="" 
+          draggable="false"
+        /> */}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='upgrade'>
+        <div className='upgradeLeft'>
+          <div className="upgradeKid" onClick={() => selfReforestation()}>
+            <div className="leftSection">
+              <Image 
+                src={imgKid}
+                width={65}
+                alt="" 
+                draggable="false"
+                className='imgKid'
+              />
+            </div>
+            <div className="midSection">
+              <h4>Self reforestation</h4>
+              <div className='cost'>
+                <p>Cost: <span className="costKid">100</span></p>
+                <Image 
+                src={imgTree}
+                width={35}
+                height={10}alt="" 
+                draggable="false"
+                />
+              </div>
+            </div>
+            <div className="rightSection">
+              <p><span className="nbPerSecondKid">1</span> Tree per second</p>
+              Quantity: <span className="nbKidUpgrade">0</span>
+            </div>
+          </div>      
+          <div className="upgradeCountry" onClick={() => countryReforestation()}>
+            <div className="leftSection">
+              <Image 
+                src={imgCountry}
+                width={85}
+                alt="" 
+                draggable="false"
+              />
+            </div>
+            <div className="CountryMidSection">
+              <h4>Country reforestation</h4>
+              <div className='cost'>
+                <p>Cost: <span className="costCountry">10 000</span></p>
+                <Image 
+                  src={imgTree}
+                  width={35}
+                  height={10}alt="" 
+                  draggable="false"
+                />
+              </div>
+            </div>
+            <div className="rightSection">
+              <p><span className="nbPerSecondCountry">150</span> Tree per second</p>
+              Quantity: <span className="nbCountryUpgrade">0</span>
+            </div>
+          </div>
+        </div>
+        <div className='upgradeRight'>
+          <div className="upgradeCity" onClick={() => cityReforestation()}>
+            <div className="leftSection">
+              <Image 
+                src={imgCity}
+                width={85}
+                alt="" 
+                draggable="false"
+              />
+            </div>
+            <div className="midSection">
+              <h4>City reforestation</h4>
+              <div className='cost'>
+                <p>Cost: <span className="costCity">1 000</span></p>
+                <Image 
+                src={imgTree}
+                width={35}
+                height={10}alt="" 
+                draggable="false"
+                />
+              </div>
+            </div>
+            <div className="rightSection">
+              <p><span className="nbPerSecondCity">15</span> Tree per second</p>
+              Quantity: <span className="nbCityUpgrade">0</span>
+            </div>
+          </div>
+          <div className="upgradePlanet" onClick={() => planetReforestation()}>
+            <div className="leftSection">
+            <Image 
+              src={imgPlanet}
+              width={85}
+              alt="" 
+              draggable="false"
+            />
+            </div>
+            <div className="planetMidSection">
+              <h4>Planet reforestation</h4>
+              <div className='cost'>
+                <p>Cost: <span className="costPlanet">100 000</span></p>
+                <Image 
+                  src={imgTree}
+                  width={35}
+                  height={10}alt="" 
+                  draggable="false"
+                />
+              </div>
+            </div>
+            <div className="rightSection">
+              <p><span className="nbPerSecondPlanet">2000</span> Tree per second</p>
+              Quantity: <span className="nbPlanetUpgrade">0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
 }
+
+
+let nbCounter : number = 0;
+let xpLevel:number=50;
+let lvl:number=1;
+
+let nbPerSecond=0;
+let nbPerSecondKid=1;
+let costKid=100;
+let nbPerSecondCity=15;
+let costCity=1_000;
+let nbPerSecondCountry:number=150;
+let costCountry:number=10_000;
+let nbPerSecondPlanet=2_000;
+let costPlanet=100_000;
+
+let nbKidUpgrade=0;
+let nbCityUpgrade=0;
+let nbCountryUpgrade=0;
+let nbPlanetUpgrade=0;
+let maxSize=0;
+let hectareSize=0;
+let nbCO2=0;
+//let circleSize=1;
+
+
+function incrementCounter() {
+  let nbCounterSection = document.querySelector('.nbCounter');
+  nbCounter=nbCounter+1;  
+  nbCounterSection.innerHTML = Math.round(nbCounter).toString();
+  
+  incrementLvl();
+  incrementXp();
+  forestMaxSize();
+  //numberOnClick();
+}
+
+function incrementLvl(){
+  let lvlSection = document.querySelector('.lvlSection');
+  if (nbCounter >= xpLevel){
+    lvlSection.innerHTML = "LEVEL " + lvl.toString();
+    lvl++;  
+  }
+}
+
+function incrementXp(){
+  let xpSection = document.querySelector('.xpSection');
+  if (nbCounter >= xpLevel){
+    xpLevel=xpLevel*2;
+    xpSection.innerHTML = "Tree to reach the next level: " + xpLevel.toString();
+  }
+}
+
+function selfReforestation(){
+  if(nbCounter>=costKid){
+    nbPerSecond+=Math.round(nbPerSecondKid);
+    nbPerSecondKid=nbPerSecondKid*1.2;
+    nbKidUpgrade++;
+    nbCounter-=costKid;
+    costKid=costKid*1.2;
+
+    let SectionbKidUpgrade = document.querySelector('.nbKidUpgrade');
+    SectionbKidUpgrade.innerHTML = nbKidUpgrade.toString();
+
+    let nbCounterSection = document.querySelector('.nbCounter');
+    nbCounterSection.innerHTML = Math.round(nbCounter).toString();
+
+    let nbPerSecondSection = document.querySelector('.nbPerSecond');
+    nbPerSecondSection.innerHTML = Math.round(nbPerSecond).toString();
+
+    let costCitySection = document.querySelector('.costKid');
+    costCitySection.innerHTML = Math.round(costKid).toString();
+
+    let nbPerSecondKidSection = document.querySelector('.nbPerSecondKid');
+    nbPerSecondKidSection.innerHTML = Math.round(nbPerSecondKid).toString();
+  }
+  else {
+
+  }
+}
+
+function cityReforestation(){
+  if(nbCounter>=costCity){
+    nbPerSecond+=Math.round(nbPerSecondCity);
+    nbPerSecondCity=nbPerSecondCity*1.2;
+    nbCityUpgrade++;
+    nbCounter-=costCity;
+    costCity=costCity*1.2;
+
+    let SectionbCityUpgrade = document.querySelector('.nbCityUpgrade');
+    SectionbCityUpgrade.innerHTML = nbCityUpgrade.toString();
+
+    let nbCounterSection = document.querySelector('.nbCounter');
+    nbCounterSection.innerHTML = Math.round(nbCounter).toString();
+
+    let nbPerSecondSection = document.querySelector('.nbPerSecond');
+    nbPerSecondSection.innerHTML = Math.round(nbPerSecond).toString();
+
+    let costCitySection = document.querySelector('.costCity');
+    costCitySection.innerHTML = Math.round(costCity).toString();
+
+    let nbPerSecondCitySection = document.querySelector('.nbPerSecondCity');
+    nbPerSecondCitySection.innerHTML = Math.round(nbPerSecondCity).toString();
+  }
+  else {
+
+  }
+}
+
+function countryReforestation(){
+  const canAffordCountryReforestation = nbCounter>=costCountry;
+  if(canAffordCountryReforestation){
+    nbPerSecond+=Math.round(nbPerSecondCountry);
+    nbPerSecondCountry=nbPerSecondCountry*1.2;
+    nbCountryUpgrade++;
+    nbCounter-=costCountry;
+    costCountry=costCountry*1.2;
+
+    let SectionbCountryUpgrade = document.querySelector('.nbCountryUpgrade');
+    SectionbCountryUpgrade.innerHTML = nbCountryUpgrade.toString();
+
+    let nbCounterSection = document.querySelector('.nbCounter');
+    nbCounterSection.innerHTML = Math.round(nbCounter).toString();
+
+    let nbPerSecondSection = document.querySelector('.nbPerSecond');
+    nbPerSecondSection.innerHTML = Math.round(nbPerSecond).toString();
+
+    let costCountrySection = document.querySelector('.costCountry');
+    costCountrySection.innerHTML = Math.round(costCountry).toString();
+
+    let nbPerSecondCountrySection = document.querySelector('.nbPerSecondCountry');
+    nbPerSecondCountrySection.innerHTML = Math.round(nbPerSecondCountry).toString();
+  }
+  else {
+
+  }
+}
+
+function planetReforestation(){
+  if(nbCounter>=costPlanet){
+    nbPerSecond+=Math.round(nbPerSecondPlanet);
+    nbPerSecondPlanet=nbPerSecondPlanet*1.2;
+    nbPlanetUpgrade++;
+    nbCounter-=costPlanet;
+    costPlanet=costPlanet*1.2;
+
+    let SectionbPlanetUpgrade = document.querySelector('.nbPlanetUpgrade');
+    SectionbPlanetUpgrade.innerHTML = nbPlanetUpgrade.toString();
+
+    let nbCounterSection = document.querySelector('.nbCounter');
+    nbCounterSection.innerHTML = Math.round(nbCounter).toString();
+
+    let nbPerSecondSection = document.querySelector('.nbPerSecond');
+    nbPerSecondSection.innerHTML = Math.round(nbPerSecond).toString();
+
+    let costPlanetSection = document.querySelector('.costPlanet');
+    costPlanetSection.innerHTML = Math.round(costPlanet).toString();
+
+    let nbPerSecondPlanetSection = document.querySelector('.nbPerSecondPlanet');
+    nbPerSecondPlanetSection.innerHTML = Math.round(nbPerSecondPlanet).toString();
+  }
+  else {
+
+  }
+}
+
+function incrementCounterSecond(){
+  let nbCounterBefore = nbCounter;
+  nbCounter=nbCounter+nbPerSecond;
+  if(nbCounter>nbCounterBefore){
+    let nbCounterSection = document.querySelector('.nbCounter');
+    nbCounterSection.innerHTML = Math.round(nbCounter).toString();
+    forestMaxSize();
+  }
+}
+setInterval(incrementCounterSecond,1000)
+
+function forestMaxSize(){
+  if(nbCounter>maxSize) {
+    maxSize=nbCounter;
+    let maxSizeSection = document.querySelector('.maxSize');
+    maxSizeSection.innerHTML = Math.round(maxSize).toString();
+    hectareSize=maxSize/500;
+    let hectareSizeSection = document.querySelector('.hectareSize');
+    hectareSizeSection.innerHTML = Math.round(hectareSize).toString();
+  }
+  CO2calcul();
+}
+
+function CO2calcul(){
+  nbCO2=maxSize*25;
+  let CO2Section = document.querySelector('.nbCO2');
+  CO2Section.innerHTML = Math.round(nbCO2).toString();
+}
+
+/* function numberOnClick(){
+  let img = document.querySelector('.img');
+
+  let clickerOffset = img.getBoundingClientRect();
+
+  let position = (event: MouseEvent) => {
+    let { clientX, clientY } = event;    
+    x: event.pageX - clickerOffset.left,
+    y: event.pageY - clickerOffset.top
+  }; 
+
+
+  let event: MouseEvent;
+  let clickPosition = { 
+    x: event.pageX - clickerOffset.left, 
+    y: event.pageY - clickerOffset.top 
+  };
+
+  let element = document.createElement("div");
+  element.textContent = "+1";
+  element.classList.add("number", "unselectable") 
+  element.style.left = clickPosition.x + "px";
+  element.style.top = clickPosition.y + "px";
+
+  img.appendChild(element);
+}   */
